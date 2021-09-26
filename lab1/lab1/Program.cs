83bd7929543323f340c6445f3a61753e92550b84
averageioenjoyer <91421527+averageioenjoyer@users.noreply.github.com>
2021-09-26 17:36:36 +0300

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integers");
+            Console.WriteLine(int.Parse(Console.ReadLine()) * int.Parse(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }
+}
